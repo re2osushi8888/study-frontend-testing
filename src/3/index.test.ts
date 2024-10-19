c0a096ac51ed1703b50ed655e04fd755d75057f1
re2osushi8888 <re.2osushi8888@gmail.com>
2024-10-19 14:39:32 +0000

    リスト3-4

--- src/3/index.test.ts
+++ src/3/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'bun:test';
-import { add } from './index';
+import { add, sub } from './index';
 
 // リスト3-2
 test('add: 1+2は3', () => {
@@ -13,5 +13,27 @@ describe('add', () => {
 	});
 	test('1+2は3', () => {
 		expect(add(1, 2)).toBe(3);
+	});
+});
+
+// リスト3-4
+describe('四則演算', () => {
+	describe('add', () => {
+		test('1+1は2', () => {
+			expect(add(1, 1)).toBe(2);
+		});
+
+		test('1+2は3', () => {
+			expect(add(1, 2)).toBe(3);
+		});
+	});
+
+	describe('sub', () => {
+		test('1-1は0', () => {
+			expect(sub(1, 1)).toBe(0);
+		});
+		test('2-1は1', () => {
+			expect(sub(2, 1)).toBe(1);
+		});
 	});
 });
