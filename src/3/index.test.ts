6813172da0fbf33ff63970f5b6d1ef4bd0f9c46f
re2osushi8888 <re.2osushi8888@gmail.com>
2024-10-19 14:37:50 +0000

    リスト3-3

--- src/3/index.test.ts
+++ src/3/index.test.ts
@@ -1,7 +1,17 @@
-import { expect, test } from 'bun:test';
+import { describe, expect, test } from 'bun:test';
 import { add } from './index';
 
-// 3-2
-test('add: 1 + 2 は 3', () => {
+// リスト3-2
+test('add: 1+2は3', () => {
 	expect(add(1, 2)).toBe(3);
+});
+
+// リスト3-3
+describe('add', () => {
+	test('1+1は2', () => {
+		expect(add(1, 1)).toBe(2);
+	});
+	test('1+2は3', () => {
+		expect(add(1, 2)).toBe(3);
+	});
 });
