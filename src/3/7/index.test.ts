126b02556ee7eca1fac25f64c54668c629d3d6a4
re2osushi8888 <re.2osushi8888@gmail.com>
2024-10-24 15:10:27 +0000

    3-7 timeout

--- src/3/7/index.test.ts
+++ src/3/7/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'bun:test';
-import { wait } from '.';
+import { timeout, wait } from '.';
 
 describe('非同期処理', () => {
 	describe('wait', () => {
@@ -11,8 +11,38 @@ describe('非同期処理', () => {
 		test('[async]指定時間待つと,経過時間をもってresolveされる', async () => {
 			await expect(wait(50)).resolves.toBe(50);
 		});
-		test('[async]指定時間待つと,経過時間を待ってresolveされる', async () => {
+		test('[async]指定時間待つと,経過時間をもってresolveされる', async () => {
 			expect(await wait(50)).toBe(50);
+		});
+	});
+	describe('timeout', () => {
+		test('[Promise]指定時間待つと,経過時間をもってrejectされる', () => {
+			return timeout(50).catch((duration) => {
+				expect(duration).toBe(50);
+			});
+		});
+		test('[async]指定時間待つと,経過時間を持ってrejectされる', async () => {
+			return expect(timeout(50)).rejects.toBe(50);
+		});
+		test('[async]指定時間待つと,経過時間を持ってrejectされる', async () => {
+			await expect(timeout(50)).rejects.toBe(50);
+		});
+		test('[try-catch]指定時間待つと,経過時間をもってrejectされる', async () => {
+			// catchを通ることを確認するためにassertionの数を確認する
+			expect.assertions(1);
+			try {
+				await timeout(50);
+			} catch (err) {
+				expect(err).toBe(50);
+			}
+		});
+		test('return していないため、Promise が解決する前にテストが終了してしまう', () => {
+			// bun:testだと両方失敗する
+
+      // 失敗を期待して書かれたアサーション
+			// expect(wait(2000)).resolves.toBe(3000);
+			// 正しくはアサーションを return する
+			// return expect(wait(2000)).resolves.toBe(3000);
 		});
 	});
 });
