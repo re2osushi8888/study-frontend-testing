35ebf506bbcf0c22fdbeedd824216923d34157f6
re2osushi8888 <re.2osushi8888@gmail.com>
2024-10-24 10:51:18 +0000

    3-6 数値の検証

--- src/3/6/index.test.ts
+++ src/3/6/index.test.ts
@@ -23,5 +23,28 @@ describe('真偽値の検証', () => {
 		expect(undefined).toBeUndefined();
 		expect(undefined).not.toBeDefined();
 	});
-	
+});
+
+describe('数値の検証', () => {
+	const value = 2 + 2;
+	test('検証値は期待値と等しい', () => {
+		expect(value).toBe(4);
+		expect(value).toEqual(4);
+	});
+	test('検証値は期待値より大きい', () => {
+		expect(value).toBeGreaterThan(3);
+		expect(value).toBeGreaterThanOrEqual(4);
+	});
+	test('検証値は期待値より小さい', () => {
+		expect(value).toBeLessThan(5);
+		expect(value).toBeLessThanOrEqual(4);
+	});
+	test('小数計算は正確ではない', () => {
+		expect(0.1 + 0.2).not.toBe(0.3);
+	});
+	test('小数計算の指定桁までを比較する', () => {
+		expect(0.1 + 0.2).toBeCloseTo(0.3);
+		expect(0.1 + 0.2).toBeCloseTo(0.3, 15);
+		expect(0.1 + 0.2).not.toBeCloseTo(0.3, 16);
+	});
 });
